0e538a429f663f2b88876d3a59eefba659a2d9d2
Nikos Vasilakis <nikos.ailo@gmail.com>
2013-12-25 11:30:30 -0500

    [Minor] Add a quickbuild script

---
+++ src/view.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+  
+##
+# 2013, Nikos Vasilakis
+# n.c.vasilakis@gmail.com
+#
+# A script to quickly build and test the application
+#
+# Usage: ./view.sh
+##
+
+valac --pkg gtk+-3.0 --pkg granite Notes.vala && ./Notes
