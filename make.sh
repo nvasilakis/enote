b1c21194696567834800074fef3249f6801519ab
Nikos Vasilakis <nikos.ailo@gmail.com>
2013-12-27 15:26:43 -0500

    [Minor] Add a snap-build and cleanup script

---
+++ make.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+  
+##
+# A script to build elementary notes 
+# from sources (or cleanup) on linux
+#
+# Usage: ./make.sh [clean]
+#
+# TODO:
+#    *  write a full-install part
+##
+
+if [[ "$1" == 'clean' ]]; then
+  for file in $(cat .gitignore); do
+    echo "removing $file"
+    rm -rf $file # No quotes for full shell expansion
+  done
+else 
+  
+  rm -r ./build # re-create build for temp's
+  mkdir ./build  && cd ./build
+  # Initiate cmake and build source; also, if
+  # everything ok, bring executable to project's root
+  cmake .. &&  make && mv ./src/Notes ../Notes
+fi
